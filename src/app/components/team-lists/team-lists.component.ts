b05b224e5d90c20c871da26d718188d2de9e3199
Matt Taylor <taylor.matt777@gmail.com>
2020-06-27 10:34:03 -0700

    Clean up team lists component

--- src/app/components/team-lists/team-lists.component.ts
+++ src/app/components/team-lists/team-lists.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Game} from '../../../../types';
 
 @Component({
@@ -6,30 +6,8 @@ import {Game} from '../../../../types';
   templateUrl: './team-lists.component.html',
   styleUrls: ['./team-lists.component.scss'],
 })
-export class TeamListsComponent implements OnInit {
+export class TeamListsComponent {
   @Input() game: Game;
 
-
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  // TODO: Wasn't sure the best way to conver these ids into their respecctive uesrs so we could diplay their names
-  get blueTeam(): string[] {
-    if (this.game){
-      return this.game.blueTeam.userIds;
-    }
-    return [];
-  }
-
-  get redTeam(): string[] {
-    if (this.game){
-      return this.game.redTeam.userIds;
-    }
-    return [];
-  }
-
-
+  constructor() {}
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+
 import {User} from '../../../../types';
 import {UserService} from '../../services/user.service';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -9,14 +10,23 @@ import {Observable} from 'rxjs';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  BASE_WIDTH = 75;
+
   @Input() userId: string;
   user$: Observable<User>;
+  style: {width: string};
 
   constructor(
-      private readonly userService: UserService
-  ) { }
+      private readonly userService: UserService,
+  ) {
+    this.style = {width: `${this.getRandomWidth()}px`};
+  }
 
   ngOnInit() {
     this.user$ = this.userService.getUser(this.userId);
+  }
+
+  getRandomWidth() {
+    return this.BASE_WIDTH + Math.floor(Math.random() * 20) + 1;
   }
 }
