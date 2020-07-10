bbe1b289d69cbed338f4f54b0ff37a2a2826c965
Matt Taylor <taylor.matt777@gmail.com>
2020-07-09 22:34:13 -0600

    Cache user observables and lazy load user images

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,18 +7,31 @@ import {User} from '../../../types';
 
 @Injectable({providedIn: 'root'})
 export class UserService {
+  userObservables: {[userId: string]: Observable<User>} = {};
+
   constructor(
       private readonly afs: AngularFirestore,
   ) {}
 
   getUser(userId: string): Observable<User> {
-    return this.afs.collection('users').doc<User>(userId).valueChanges().pipe(
-        map(user => {
-          if (user === undefined) {
-            user = {name: userId, email: userId, rooms: []};
-          }
-          user.rooms = user.rooms || [];
-          return user;
-        }));
+    // load from cache when we can
+    if (this.userObservables[userId]) {
+      return this.userObservables[userId];
+    }
+
+    // otherwise query for the user
+    const user$ =
+        this.afs.collection('users').doc<User>(userId).valueChanges().pipe(
+            map(user => {
+              if (user === undefined) {
+                user = {name: userId, email: userId, rooms: []};
+              }
+              user.rooms = user.rooms || [];
+              return user;
+            }));
+
+    // save to cache and return
+    this.userObservables[userId] = user$;
+    return user$;
   }
 }
