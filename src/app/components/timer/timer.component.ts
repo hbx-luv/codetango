c87bd1e516b57568d53354a5915b1eb247dea6ff
Matt Taylor <taylor.matt777@gmail.com>
2020-09-30 19:36:26 -0600

    Show the timer after games are finished

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -10,6 +10,7 @@ import {GameService} from '../../services/game.service';
 export class TimerComponent {
   @Input() room: Room;
   @Input() game: Game;
+
   clock = '';
   seconds: number;
   timeout: any;  // NodeJS.Timeout;
@@ -36,15 +37,24 @@ export class TimerComponent {
       this.timerTooltip = this.room.enforceTimer ?
           'When the timer runs out, the turn will automaticlly be ended.' :
           'The timer here is just a suggestion, nothing will happen when the timer runs out.';
+    } else {
+      this.timerTooltip = 'The time remaining on the clock';
     }
   }
 
   calculateClock() {
-    if (!this.game.completedAt && this.game.turnEnds) {
-      this.seconds = (this.game.turnEnds - Date.now()) / 1000;
+    const {completedAt, turnEnds} = this.game;
+
+    // if the turn is meant to end at some time, show the timer
+    if (turnEnds) {
+      // the timer is relative to when the game was completed for finished games
+      const timerRelativeTo = completedAt || Date.now();
+      this.seconds = (turnEnds - timerRelativeTo) / 1000;
       this.clock = this.fancyTimeFormat(this.seconds);
 
-      if (this.seconds <= 0 && this.room.enforceTimer) {
+      // TODO: re-implement enforceTimer in a way that ensures the turn is only
+      // ended once
+      if (this.seconds <= 0 && this.room.enforceTimer && !completedAt) {
         this.gameService.updateGame(this.game.id, {
           status: this.game.status === GameStatus.BLUES_TURN ?
               GameStatus.REDS_TURN :
@@ -53,9 +63,12 @@ export class TimerComponent {
         });
       }
 
-      // determine the amount of time to the end of the second
-      const timeToWait = 1000 - (Date.now() % 1000);
-      this.timeout = setTimeout(this.calculateClock.bind(this), timeToWait);
+      // if the game isn't completed, update the timer in about a second
+      if (!completedAt) {
+        // determine the amount of time to the end of the second
+        const timeToWait = 1000 - (Date.now() % 1000);
+        this.timeout = setTimeout(this.calculateClock.bind(this), timeToWait);
+      }
     }
   }
 
